f2e8bd6ec3d802187af3e4cbb959d34d555d985b
jds1985 <jimsaenz1985@yahoo.com>
2026-05-06 00:47:04 -0400

    Refactor comments in Scene.js for clarity

--- components/Scene.js
+++ components/Scene.js
@@ -6,7 +6,6 @@ export default function Scene() {
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas camera={{ position: [0, 2, 5], fov: 60 }}>
 
-        {/* LIGHT */}
         <ambientLight intensity={1} />
 
         {/* FLOOR */}
@@ -15,19 +14,18 @@ export default function Scene() {
           <meshStandardMaterial color="#2e2e2e" />
         </mesh>
 
-        {/* BACK WALL */}
+        {/* WALL */}
         <mesh position={[0, 5, -10]}>
           <planeGeometry args={[20, 10]} />
           <meshStandardMaterial color="#4b3b34" />
         </mesh>
 
-        {/* TEST PAINTING */}
+        {/* TEST ART */}
         <mesh position={[0, 4, -9.8]}>
           <planeGeometry args={[3, 2]} />
           <meshStandardMaterial color="white" />
         </mesh>
 
-        {/* CAMERA CONTROL */}
         <OrbitControls
           enableZoom={false}
           enablePan={false}
